59e6fc6b59fa25a0a235135353b7d680028a8331
prasad godase <prasadgodase5@gmail.com>
2023-05-14 20:32:34 +0530

    Variable_aasignment
    Variable_aasignment

---
+++ 01.1_variable_assign.js
@@ -0,0 +1,30 @@
+var myFullName;  //Variable Declaration
+console.log(myFullName);
+myFullName = "Prasad Godase"; //Variable Initialization
+console.log(myFullName);
+myFullName ="Prasad Ananda Godase" //Variable Update
+console.log(myFullName);
+
+
+var myLove = "I love only JS"; //Variable declaration and Initialization
+console.log(myLove);
+
+var firstName = "Prasad"
+console.log(firstName);
+
+var lastName = "Godase";
+console.log(lastName);
+
+var age ="22";
+console.log(age);
+
+var address = "Solapur,Sangola";
+console.log(address);
+
+var pincode ="413307";
+console.log(pincode); 
+var gender= 'Male'; 
+console.log(gender);
+
+var isMarried = "No"
+console.log(isMarried);
